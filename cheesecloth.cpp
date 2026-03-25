ddeb209bde0299e4c139e87540752e0ae54f2a39
John Bain <bainj288@lynchburg.edu>
2026-03-24 20:59:39 -0400

    Grant and John worked on implementing pthreads for isOdd

--- cheesecloth.cpp
+++ cheesecloth.cpp
@@ -3,9 +3,13 @@
 #include <pthread.h>
 using namespace std;
 
+//Prototypes
 bool isOdd(unsigned long long number);
 bool isFib(unsigned long long number);
 bool reachesForty(unsigned long long number);
+void* threadInput(void* nothing);
+void* threadOdd(void* nothing);
+
 
 //Global Variable
 queue<unsigned long long> queueOne;
@@ -13,6 +17,10 @@ queue<unsigned long long> queueTwo;
 queue<unsigned long long> queueThree;
 queue<unsigned long long> queueFour;
 
+bool inputFinished;
+bool oddThreadFinished;  
+
+//Threads
 pthread_mutex_t mutexOne = PTHREAD_MUTEX_INITIALIZER; 
 pthread_mutex_t mutexTwo = PTHREAD_MUTEX_INITIALIZER; 
 pthread_mutex_t mutexThree = PTHREAD_MUTEX_INITIALIZER; 
@@ -79,3 +87,60 @@ bool reachesForty(unsigned long long number)
 
     return result; 
 }
+
+void* threadInput(void* nothing)
+{
+    unsigned long long number; 
+    bool done = false;
+    while((cin >> number) && !done)
+    {
+        if(number == 0)
+        {
+            done = true;
+        } 
+        else
+        {
+            pthread_mutex_lock(&mutexOne);
+            queueOne.push(number);
+            pthread_mutex_unlock(&mutexOne);
+
+        }       
+    }
+    inputFinished = true; 
+    return nullptr;
+}
+
+void* threadOdd(void* nothing)
+{
+    bool done = false; 
+    while(!done)
+    {
+        pthread_mutex_lock(&mutexOne);
+        if(!queueOne.empty())
+        {
+            unsigned long long number = queueOne.front(); 
+            queueOne.pop();
+            pthread_mutex_unlock(&mutexOne);
+            if(isOdd(number))
+            {
+                pthread_mutex_lock(&mutexTwo);
+                queueTwo.push(number);
+                pthread_mutex_unlock(&mutexTwo);
+
+            }
+    
+        }
+        //Spin lock
+        else
+        {
+            pthread_mutex_unlock(&mutexOne);
+            if(inputFinished)
+            {
+                done = true;
+            }
+
+        }   
+    }
+    oddThreadFinished = true;
+    return nullptr;
+} 
