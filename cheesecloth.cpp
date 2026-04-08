8720836c79a37a53de33ccebd5ceee28195dcbcd
Grant Howard <granthoward04@gmail.com>
2026-04-08 11:28:13 -0400

    Added strerror

--- cheesecloth.cpp
+++ cheesecloth.cpp
@@ -2,6 +2,7 @@
 #include <queue>
 #include <pthread.h>
 #include <list>
+#include <cstring>
 using namespace std;
 
 //Prototypes
@@ -13,6 +14,7 @@ void* threadOdd(void* nothing);
 void* threadFib(void* nothing);
 void* threadCollatz(void* nothing);
 void* threadOutput(void* nothing);
+void handleError(int returnCode, const char* errorMessage);
 
 
 //Global Variable
@@ -47,6 +49,17 @@ pthread_cond_t conditionTwo = PTHREAD_COND_INITIALIZER;
 pthread_cond_t conditionThree = PTHREAD_COND_INITIALIZER;
 pthread_cond_t conditionFour = PTHREAD_COND_INITIALIZER;
 
+// helper for pthread errors
+void handleError(int returnCode, const char* message)
+{
+    if(returnCode != 0)
+    {
+        char buffer[256];
+        strerror_r(returnCode, buffer, sizeof(buffer));
+        cerr << message << " : " << buffer << endl;
+        exit(1);
+    }
+}
 
 int main(int argc, char* argv[])
 {
@@ -78,45 +91,45 @@ int main(int argc, char* argv[])
             list<pthread_t> collatzThreads;
 
             // start the input thread
-            pthread_create(&inputThread, nullptr, threadInput, nullptr);
+            handleError(pthread_create(&inputThread, nullptr, threadInput, nullptr), "Error creating input thread");
 
             // create the threads
             for(int i = 0; i < numberThreads; i++)
             {
                 pthread_t t1, t2, t3;
 
-                pthread_create(&t1, nullptr, threadOdd, nullptr);
+                handleError(pthread_create(&t1, nullptr, threadOdd, nullptr), "Error creating odd thread");
                 oddThreads.push_back(t1);
 
-                pthread_create(&t2, nullptr, threadFib, nullptr);
+                handleError(pthread_create(&t2, nullptr, threadFib, nullptr), "Error creating fib thread");
                 fibThreads.push_back(t2);
 
-                pthread_create(&t3, nullptr, threadCollatz, nullptr);
+                handleError(pthread_create(&t3, nullptr, threadCollatz, nullptr), "Error creating collatz thread");
                 collatzThreads.push_back(t3);
             }
 
             // start output thread
-            pthread_create(&outputThread, nullptr, threadOutput, nullptr);
+            handleError(pthread_create(&outputThread, nullptr, threadOutput, nullptr), "Error creating output thread");
 
             // wait for all threads to finish with join
-            pthread_join(inputThread, nullptr);
+            handleError(pthread_join(inputThread, nullptr), "Error joining input thread");
 
             for(const auto& t : oddThreads)
             {
-                pthread_join(t, nullptr);
+                handleError(pthread_join(t, nullptr), "Error joining odd thread");
             }
 
             for(const auto& t : fibThreads)
             {
-                pthread_join(t, nullptr);
+                handleError(pthread_join(t, nullptr), "Error joining fib threads");
             }
 
             for(const auto& t : collatzThreads)
             {
-                pthread_join(t, nullptr);
+                handleError(pthread_join(t, nullptr), "Error joining collatz threads");
             }
 
-            pthread_join(outputThread, nullptr);
+            handleError(pthread_join(outputThread, nullptr), "Error joining output thread");
 
         }
         
@@ -199,21 +212,21 @@ void* threadInput(void* nothing)
         } 
         else
         {
-            pthread_mutex_lock(&mutexOne);
+            handleError(pthread_mutex_lock(&mutexOne), "Error locking mutexOne");
             queueOne.push(number);
 
             // wake one odd thread
-            pthread_cond_signal(&conditionOne);
-            pthread_mutex_unlock(&mutexOne);
+            handleError(pthread_cond_signal(&conditionOne), "Error signaling conditionOne");
+            handleError(pthread_mutex_unlock(&mutexOne), "Error unlocking mutexOne");
 
         }       
     }
 
     // mark input as finished and wake all the other threads
-    pthread_mutex_lock(&mutexOne);
+    handleError(pthread_mutex_lock(&mutexOne), "Error locking mutexOne");
     inputFinished = true; 
-    pthread_cond_broadcast(&conditionOne);
-    pthread_mutex_unlock(&mutexOne);
+    handleError(pthread_cond_broadcast(&conditionOne), "Error broadcasting conditionOne");
+    handleError(pthread_mutex_unlock(&mutexOne), "Error unlocking mutexOne");
 
     return nullptr;
 }
@@ -224,12 +237,12 @@ void* threadOdd(void* nothing)
     bool done = false; 
     while(!done)
     {
-        pthread_mutex_lock(&mutexOne);
+        handleError(pthread_mutex_lock(&mutexOne), "Error locking mutexOne");
 
         // wait while there is nothing to do and input is not done
         while(queueOne.empty() && !inputFinished)
         {
-            pthread_cond_wait(&conditionOne, &mutexOne);
+            handleError(pthread_cond_wait(&conditionOne, &mutexOne), "Error waiting on conditionOne");
         }
 
         // if queue is empty and input thread is done, we can proceed
@@ -244,9 +257,9 @@ void* threadOdd(void* nothing)
 
                 oddThreadFinished = true;
 
-                pthread_mutex_lock(&mutexTwo);
-                pthread_cond_broadcast(&conditionTwo);
-                pthread_mutex_unlock(&mutexTwo);
+                handleError(pthread_mutex_lock(&mutexTwo), "Error locking mutexTwo");
+                handleError(pthread_cond_broadcast(&conditionTwo), "Error broadcasting conditionTwo");
+                handleError(pthread_mutex_unlock(&mutexTwo), "Error unlocking mutexTwo");
             }
 
             pthread_mutex_unlock(&mutexOne);
@@ -257,16 +270,16 @@ void* threadOdd(void* nothing)
             // remove one item from queueOne
             unsigned long long number = queueOne.front(); 
             queueOne.pop();
-            pthread_mutex_unlock(&mutexOne);
+            handleError(pthread_mutex_unlock(&mutexOne), "Error unlocking mutexOne");
 
             if(isOdd(number))
             {
-                pthread_mutex_lock(&mutexTwo);
+                handleError(pthread_mutex_lock(&mutexTwo), "Error locking mutexTwo");
                 queueTwo.push(number);
 
                 // wake up one fib thread 
-                pthread_cond_signal(&conditionTwo);
-                pthread_mutex_unlock(&mutexTwo);
+                handleError(pthread_cond_signal(&conditionTwo), "Error signaling conditionTwo");
+                handleError(pthread_mutex_unlock(&mutexTwo), "Error unlocking mutexTwo");
 
             }
         }
@@ -283,12 +296,12 @@ void* threadFib(void* nothing)
 
     while(!done)
     {
-        pthread_mutex_lock(&mutexTwo);
+        handleError(pthread_mutex_lock(&mutexTwo), "Error locking mutexTwo");
 
         // wait while there is nothing to do and the odd phase may still have work
         while(queueTwo.empty() && !oddThreadFinished)
         {
-            pthread_cond_wait(&conditionTwo, &mutexTwo);
+            handleError(pthread_cond_wait(&conditionTwo, &mutexTwo), "Error waiting on conditionTwo");
         }
 
         // if queueTwo is empty and the odd phase is finished then fib thread can finish
@@ -302,12 +315,12 @@ void* threadFib(void* nothing)
             {
                 fibThreadFinished = true;
 
-                pthread_mutex_lock(&mutexThree);
-                pthread_cond_broadcast(&conditionThree);
-                pthread_mutex_unlock(&mutexThree);
+                handleError(pthread_mutex_lock(&mutexThree), "Error locking mutexThree");
+                handleError(pthread_cond_broadcast(&conditionThree), "Error broadcasting conditionThree");
+                handleError(pthread_mutex_unlock(&mutexThree), "Error unlocking mutexThree");
             }
 
-            pthread_mutex_unlock(&mutexTwo);
+            handleError(pthread_mutex_unlock(&mutexTwo), "Error unlocking mutexTwo");
             done = true;
         }
         else
@@ -315,16 +328,16 @@ void* threadFib(void* nothing)
             // remove one item from queueTwo
             unsigned long long number = queueTwo.front();
             queueTwo.pop();
-            pthread_mutex_unlock(&mutexTwo);
+            handleError(pthread_mutex_unlock(&mutexTwo), "Error unlocking mutexTwo");
 
             if(isFib(number))
             {
-                pthread_mutex_lock(&mutexThree);
+                handleError(pthread_mutex_lock(&mutexThree), "Error locking mutexThree");
                 queueThree.push(number);
 
                 // wake a collatz thread because it has work to do
-                pthread_cond_signal(&conditionThree);
-                pthread_mutex_unlock(&mutexThree);
+                handleError(pthread_cond_signal(&conditionThree), "Error signaling conditionThree");
+                handleError(pthread_mutex_unlock(&mutexThree), "Error unlocking mutexThree");
             }
         }
         
@@ -339,13 +352,13 @@ void* threadCollatz(void* nothing)
 
     while(!done)
     {
-        pthread_mutex_lock(&mutexThree);
+        handleError(pthread_mutex_lock(&mutexThree), "Error locking mutexThree");
 
 
         // wait while there is nothing to do and the fib phase is not done
         while(queueThree.empty() && !fibThreadFinished)
         {
-            pthread_cond_wait(&conditionThree, &mutexThree);
+            handleError(pthread_cond_wait(&conditionThree, &mutexThree), "Error waiting on conditionThree");
         }
 
         // if queueThree is empty and fib phase is done then this thread can finish
@@ -359,12 +372,12 @@ void* threadCollatz(void* nothing)
             {
                 collatzThreadFinished = true; 
 
-                pthread_mutex_lock(&mutexFour);
-                pthread_cond_broadcast(&conditionFour);
-                pthread_mutex_unlock(&mutexFour);
+                handleError(pthread_mutex_lock(&mutexFour), "Error locking mutexFour");
+                handleError(pthread_cond_broadcast(&conditionFour), "Error broadcasting conditionFour");
+                handleError(pthread_mutex_unlock(&mutexFour), "Error unlocking mutexFour");
             }
 
-            pthread_mutex_unlock(&mutexThree);
+            handleError(pthread_mutex_unlock(&mutexThree), "Error unlocking mutexThree");
             done = true;
         }
         else
@@ -373,19 +386,19 @@ void* threadCollatz(void* nothing)
             unsigned long long number = queueThree.front();
             queueThree.pop();
 
-            pthread_mutex_unlock(&mutexThree);
+            handleError(pthread_mutex_unlock(&mutexThree), "Error unlocking mutexThree");
 
             // check whether number reaches 40
             // this uses the Collatz procedure
             if(reachesForty(number))
             {
                 // if it passes then we want to move it to the output queue
-                pthread_mutex_lock(&mutexFour);
+                handleError(pthread_mutex_lock(&mutexFour), "Error locking mutexFour");
                 queueFour.push(number);
 
                 // wake output thread because there is something to print
-                pthread_cond_signal(&conditionFour);
-                pthread_mutex_unlock(&mutexFour);
+                handleError(pthread_cond_signal(&conditionFour), "Error signaling conditionFour");
+                handleError(pthread_mutex_unlock(&mutexFour), "Error unlocking mutexFour");
             }
         }
         
@@ -401,18 +414,18 @@ void* threadOutput(void* nothing)
 
     while(!done)
     {
-        pthread_mutex_lock(&mutexFour);
+        handleError(pthread_mutex_lock(&mutexFour), "Error locking mutexFour");
 
         // wait while there is nothing to print and collatz phase is not done
         while(queueFour.empty() && !collatzThreadFinished)
         {
-            pthread_cond_wait(&conditionFour, &mutexFour);
+            handleError(pthread_cond_wait(&conditionFour, &mutexFour), "Error waiting on conditionFour");
         }
 
         // if queueFour is empty and collatz phase is done then output thread can finish
         if(queueFour.empty() && collatzThreadFinished)
         {
-            pthread_mutex_unlock(&mutexFour);
+            handleError(pthread_mutex_unlock(&mutexFour), "Error unlocking mutexFour");
             done = true;
         }
         else
@@ -421,7 +434,7 @@ void* threadOutput(void* nothing)
             unsigned long long number = queueFour.front();
             queueFour.pop();
 
-            pthread_mutex_unlock(&mutexFour);
+            handleError(pthread_mutex_unlock(&mutexFour), "Error unlocking mutexFour");
 
             // print the number
             cout << number << endl;
